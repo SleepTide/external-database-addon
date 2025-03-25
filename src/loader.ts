225972277ff6be70298d2c5c5fd342c85d13f0da
SleepTide <tidesleep@gmail.com>
2025-03-25 19:34:32 +0000

    updated to 1.21.70

--- src/loader.ts
+++ src/loader.ts
@@ -4,14 +4,14 @@ import API from "./Utils/API/API";
 new API();
 
 async function test() {
-    // const request = await API.CreateMember({
-    //     entity_id: "1",
-    //     username: "Sleeper Tide",
-    // });
-    // console.log(JSON.stringify(request, null, 2));
-    // const request = await API.SearchMember("Sleeper Tide");
-    // console.log(JSON.stringify(request, null, 2));
-    // const request = await API.DeleteMember("1");
-    // console.log(JSON.stringify(request, null, 2));
+  // const request = await API.CreateMember({
+  //     entity_id: "1",
+  //     username: "Sleeper Tide",
+  // });
+  // console.log(JSON.stringify(request, null, 2));
+  // const request = await API.SearchMember("Sleeper Tide");
+  // console.log(JSON.stringify(request, null, 2));
+  // const request = await API.DeleteMember("1");
+  // console.log(JSON.stringify(request, null, 2));
 }
 test();
